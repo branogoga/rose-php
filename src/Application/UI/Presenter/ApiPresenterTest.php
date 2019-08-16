a737d9a4d1cec497789d809d25aa617cb3e08ada
Branislav Goga <brano.goga@centrum.sk>
2019-08-16 12:34:22 +0200

    Add test of presenter run.

--- src/Application/UI/Presenter/ApiPresenterTest.php
+++ src/Application/UI/Presenter/ApiPresenterTest.php
@@ -20,7 +20,37 @@ final class ApiPresenterTest extends PHPUnit\Framework\TestCase
 {
 	public function		testFoo()
 	{
+        $factory = new \Nette\Application\PresenterFactory();
+        //$presenter = $factory->createPresenter("MockApi");
         $presenter = new MockApiPresenter();
+
+        /*\Nette\Application\Container*/ $context = null;
+        /*\Nette\Application\IPresenterFactory*/ $presenterFactory = null;
+        /*\Nette\Application\Router*/ $router = new \Nette\Application\Routers\SimpleRouter();
+        /*\Nette\Application\IRequest*/ $httpRequest = new \Nette\Http\Request(
+            new \Nette\Http\UrlScript(), null, null, null, null, \Nette\Http\IRequest::GET
+        );
+        /*\Nette\Application\IResponse*/ $httpResponse = new \Nette\Http\Response();
+        /*\Nette\Application\Session*/ $session = null;
+        /*\Nette\Application\User*/ $user = null;
+        /*\Nette\Application\ITemplateFactory*/ $templateFactory = null;
+
+        $presenter->injectPrimary(
+            $context,
+            $presenterFactory,
+            $router,
+            $httpRequest,
+            $httpResponse,
+            $session,
+            $user,
+            $templateFactory
+        );
+
         $this->assertNotNull($presenter);
+
+        $request = new \Nette\Application\Request("list", \Nette\Http\IRequest::GET);
+        $response = $presenter->run($request);
+
+        \Tracy\Debugger::dump($response);
 	}        
 }
