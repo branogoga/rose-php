b71917d76dc697d35e069591d17307208bd58684
Branislav Goga <brano.goga@centrum.sk>
2019-08-16 09:46:05 +0200

    Add Utils\String\Charset

---
+++ src/Utils/Strings/Charset.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rose\Utils\Strings;
+
+final class Charset
+{
+	const	CHARSET_NO_PUNCTUATION	=	"aaabccddeeefghiijklllmnnoooorrrssttuuuvxyyzzAAABCCDDEEEFGHIIJKLMNNOOOORRRSSTTUUUUVXYYZZ";
+	const	CHARSET_ISO8859_2 	    =	"aáäbcčdďeéěfghiíjklľĺmnňoóöôrŕřsštťuúüvxyýzžAÁÄBCČDĎEÉĚFGHIÍJKLMNŇOÓÔÖRŔŘSŠTŤUÚŮÜVXYÝZŽ";
+	
+	public static function removePunctuation(string $text): string
+	{
+		$text = \Nette\Utils\Strings::toAscii($text);
+		$text = str_replace(
+            array("'","`",'"',"^"), 
+            array("","","",""),
+            $text
+        );
+		
+		return $text;
+	}
+	
+    public static function  makePunctuationInsensitiveSearchRegularExpression( string $text ): string
+    {
+        $text=self::removePunctuation($text);
+        
+        $text = str_replace(array("\\"), array(""),$text);  
+        
+        $arr_conv['a'] = '[aáäAÁÄ]';
+        $arr_conv['c'] = '[cčCČ]';
+        $arr_conv['d'] = '[dďDĎ]';
+        $arr_conv['e'] = '[eéěëEÉĚË]';
+        $arr_conv['i'] = '[iíIÍ]';
+        $arr_conv['l'] = '[lĺľLĹĽ]';
+        $arr_conv['n'] = '[nňNŇ]';
+        $arr_conv['o'] = '[oóöôOÓÖÔ]';
+        $arr_conv['r'] = '[rřŕRŘŔ]';
+        $arr_conv['s'] = '[sšSŠ]';
+        $arr_conv['t'] = '[tťTŤ]';
+        $arr_conv['u'] = '[uúůüUÚŮÜ]';
+        $arr_conv['y'] = '[yýYÝ]';
+        $arr_conv['z'] = '[zžZŽ]';
+        $arr_conv['?'] = '[aáäbcčdďeéěfghiíjklľĺmnňoóöôrŕřsštťuúüvxyýzžAÁÄBCČDĎEÉĚFGHIÍJKLMNŇOÓÔÖRŔŘSŠTŤUÚŮÜVXYÝZŽ]';
+
+        $result = strtr(strtolower($text), $arr_conv);
+
+        return $result;	
+    }
+    
+    public static function removeSpaces(string $text): string
+    {
+        return strtr($text," ","_");
+    }
+}
---
+++ src/Utils/Strings/CharsetTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+require_once("Charset.php");
+
+final class CharsetTest extends PHPUnit\Framework\TestCase
+{
+	public function		testRemovePunctuation()
+	{
+		$this->assertEquals( 
+            \Rose\Utils\Strings\Charset::CHARSET_NO_PUNCTUATION,
+            \Rose\Utils\Strings\Charset::removePunctuation( \Rose\Utils\Strings\Charset::CHARSET_ISO8859_2 )
+        );
+
+		$this->assertEquals( 
+            "pajstun",
+            \Rose\Utils\Strings\Charset::removePunctuation( "pajštún" )
+        );
+
+		$this->assertEquals( 
+            "sturovo",
+            \Rose\Utils\Strings\Charset::removePunctuation( "štúrovo" )
+        );
+	}
+        
+    public function         testPunctuationInsensitiveSearchRegularExpression()
+    {
+        $this->assertEquals( 
+            "[aáäAÁÄ]",
+            \Rose\Utils\Strings\Charset::makePunctuationInsensitiveSearchRegularExpression( "a" )
+        );
+
+        $this->assertEquals( 
+            "[aáäAÁÄ]",
+            \Rose\Utils\Strings\Charset::makePunctuationInsensitiveSearchRegularExpression( "Ä" )
+        );
+    }
+
+    public function testRemoveSpaces()
+    {
+        $this->assertEquals( 
+            "",
+            \Rose\Utils\Strings\Charset::removeSpaces( "" )
+        );
+
+        $this->assertEquals( 
+            "no-spaces",
+            \Rose\Utils\Strings\Charset::removeSpaces( "no-spaces" )
+        );
+
+        $this->assertEquals( 
+            "_some_spaces_",
+            \Rose\Utils\Strings\Charset::removeSpaces( " some spaces " )
+        );
+
+        $this->assertEquals( 
+            "__more__spaces__",
+            \Rose\Utils\Strings\Charset::removeSpaces( "  more  spaces  " )
+        );
+
+    }
+}
+
